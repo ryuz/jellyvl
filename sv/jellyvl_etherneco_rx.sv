f85685bc7263649c13d4472610998639504bf047
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-03-13 00:15:19 +0900

    update

---
+++ sv/jellyvl_etherneco_rx.sv
@@ -0,0 +1,152 @@
+module jellyvl_etherneco_rx #() (
+    input logic reset,
+    input logic clk  ,
+
+    output logic rx_start,
+    output logic rx_end  ,
+    output logic rx_error,
+
+    input logic         s_first,
+    input logic         s_last ,
+    input logic [8-1:0] s_data ,
+    input logic         s_valid,
+
+    output logic         m_first,
+    output logic         m_last ,
+    output logic [8-1:0] m_data ,
+    output logic         m_valid
+);
+    typedef 
+    enum logic [5-1:0] {
+        STATE_IDLE = 5'b00000,
+        STATE_LENGTH = 5'b00010,
+        STATE_PAYLOAD = 5'b00100,
+        STATE_FCS = 5'b01000,
+        STATE_ERROR = 5'b10000
+    } STATE;
+
+    localparam type t_length = logic [16-1:0];
+
+    STATE    state     ;
+    t_length count     ;
+    t_length length    ;
+    logic    preamble  ;
+    logic    crc_update;
+
+    t_length count_next;
+    assign count_next = count + 1'b1;
+
+    always_ff @ (posedge clk) begin
+        if (reset) begin
+            rx_start <= 1'b0;
+            rx_end   <= 1'b0;
+            rx_error <= 1'b0;
+
+            m_first <= 'x;
+            m_last  <= 'x;
+            m_data  <= 'x;
+            m_valid <= 1'b0;
+
+            state      <= STATE_IDLE;
+            count      <= 'x;
+            length     <= 'x;
+            preamble   <= 1'b0;
+            crc_update <= 'x;
+        end else begin
+            rx_start <= 1'b0;
+            rx_end   <= 1'b0;
+            rx_error <= 1'b0;
+
+            if (s_valid) begin
+                count <= count_next;
+
+                case (state)
+                    STATE_IDLE: begin
+                        if (preamble && (s_data == 8'hd5) && (count >= 6 && count <= 8)) begin
+                            state      <= STATE_LENGTH;
+                            count      <= 16'd1;
+                            crc_update <= 1'b0;
+                        end
+                        m_first <= 1'bx;
+                        m_last  <= 1'bx;
+                    end
+
+                    STATE_LENGTH: begin
+                        if (count[0]) begin
+                            length[7:0] <= s_data;
+                            crc_update  <= 1'b1;
+                            m_first     <= 1'bx;
+                            m_last      <= 1'bx;
+                        end else begin
+                            state        <= STATE_PAYLOAD;
+                            count        <= 16'd1;
+                            length[15:8] <= s_data;
+                            m_first      <= 1'b1;
+                            m_last       <= ({s_data, length[7:0]} == 16'd1);
+                        end
+                    end
+
+                    STATE_PAYLOAD: begin
+                        m_first <= 1'b0;
+                        m_last  <= (count_next == length);
+                        if (m_last) begin
+                            state  <= STATE_FCS;
+                            count  <= 16'd1;
+                            length <= 'x;
+                        end
+                    end
+
+                    STATE_FCS: begin
+                        if (count == 3'd3) begin
+                            state <= STATE_IDLE;
+                        end
+                    end
+
+                    default: begin
+                        state <= STATE_IDLE;
+                    end
+                endcase
+
+                if (s_data != 8'h55) begin
+                    preamble <= 1'b0;
+                end
+
+                if (s_first) begin
+                    count    <= 16'd1;
+                    preamble <= (s_data == 8'h55);
+                    rx_start <= (state == STATE_IDLE);
+                end
+
+                if ((s_first && state != STATE_IDLE && state != STATE_ERROR) || (s_last && state != STATE_IDLE && state != STATE_ERROR)) begin
+                    state   <= STATE_ERROR;
+                    m_first <= 'x;
+                    m_last  <= 'x;
+                    m_data  <= 'x;
+                    m_valid <= 1'b0;
+                end
+            end
+        end
+    end
+
+    logic [32-1:0] crc_value;
+
+    jelly2_calc_crc #(
+        .DATA_WIDTH (8           ),
+        .CRC_WIDTH  (32          ),
+        .POLY_REPS  (32'h04C11DB7),
+        .REVERSED   (0           )
+    ) u_cacl_crc (
+        .reset (reset),
+        .clk   (clk  ),
+        .cke   (1'b1 ),
+        .
+        in_update (crc_update),
+        .in_data   (s_data    ),
+        .in_valid  (s_valid   ),
+        .
+        out_crc (crc_value)
+    );
+
+    // crc_value == 0x2144df1c
+
+endmodule
--- sv/jellyvl_etherneco_tx.sv
+++ sv/jellyvl_etherneco_tx.sv
@@ -2,10 +2,10 @@ module jellyvl_etherneco_tx (
     input logic reset,
     input logic clk  ,
 
-    input logic          in_start ,
-    input logic [16-1:0] in_length,
+    input logic          tx_start ,
+    input logic [16-1:0] tx_length, // 転送サイズより1小さい値を指定(AXI方式)
 
-    input logic in_cancel,
+    input logic tx_cancel,
 
     input  logic         s_last ,
     input  logic [8-1:0] s_data ,
@@ -30,161 +30,282 @@ module jellyvl_etherneco_tx (
     } STATE;
 
     localparam type t_length = logic [16-1:0];
+    localparam type t_count  = logic [3-1:0];
 
     logic cke;
     assign cke = !m_valid || m_ready;
 
-    STATE            state     ;
-    t_length         count     ;
-    t_length         length    ;
-    logic            crc_update;
-    logic            tx_first  ;
-    logic            tx_last   ;
-    logic    [8-1:0] tx_data   ;
-    logic            tx_valid  ;
 
-    t_length count_next;
-    assign count_next = count + 1'b1;
+    // ----------------------------
+    //  stage 0
+    // ----------------------------
+
+    STATE    st0_state ;
+    t_count  st0_count ;
+    t_length st0_length;
+    logic    st0_first ;
+    logic    st0_last  ;
+
+    t_count st0_count_next;
+    assign st0_count_next = st0_count + 1'b1;
+
+    t_length st0_length_next;
+    assign st0_length_next = st0_length - 1'b1;
 
     always_ff @ (posedge clk) begin
         if (reset) begin
-            state      <= STATE_IDLE;
-            count      <= 'x;
-            length     <= 'x;
-            crc_update <= 'x;
-            tx_first   <= 'x;
-            tx_last    <= 'x;
-            tx_data    <= 'x;
-            tx_valid   <= '0;
-        end else if (cke) begin
-            if (tx_valid) begin
-                count <= count_next;
-            end
+            st0_state  <= STATE_IDLE;
+            st0_count  <= 'x;
+            st0_length <= 'x;
+            st0_first  <= 1'bx;
+            st0_last   <= 1'bx;
 
-            case (state)
+        end else if (cke) begin
+
+            st0_count <= st0_count_next;
+            case (st0_state)
                 STATE_IDLE: begin
-                    count      <= 'x;
-                    length     <= in_length;
-                    crc_update <= 'x;
-                    tx_first   <= 1'bx;
-                    tx_last    <= 1'bx;
-                    tx_data    <= 8'hxx;
-                    tx_valid   <= 1'b0;
-                    if (in_start) begin
-                        state    <= STATE_PREAMBLE;
-                        count    <= '0;
-                        length   <= in_length;
-                        tx_first <= 1'b1;
-                        tx_last  <= 1'b0;
-                        tx_data  <= 8'h55;
-                        tx_valid <= 1'b1;
+                    st0_count  <= 'x;
+                    st0_length <= 'x;
+                    st0_first  <= 1'bx;
+                    st0_last   <= 1'bx;
+                    if (tx_start) begin
+                        st0_state  <= STATE_PREAMBLE;
+                        st0_count  <= '0;
+                        st0_length <= tx_length;
+                        st0_first  <= 1'b0;
+                        st0_last   <= 1'b0;
                     end
                 end
 
                 STATE_PREAMBLE: begin
-                    crc_update <= 'x;
-                    tx_first   <= 1'b0;
-                    tx_last    <= 1'b0;
-                    tx_data    <= 8'h55;
-                    tx_valid   <= 1'b1;
-                    if (count[3:0] == 3'd6) begin
-                        state    <= STATE_LENGTH;
-                        count    <= '0;
-                        tx_first <= 1'b0;
-                        tx_last  <= 1'b0;
-                        tx_data  <= 8'hd5;
-                        tx_valid <= 1'b1;
+                    st0_first <= 1'b0;
+                    st0_last  <= 1'b0;
+                    if (st0_count == 3'd6) begin
+                        st0_state <= STATE_LENGTH;
+                        st0_count <= '0;
+                        st0_first <= 1'b1;
+                        st0_last  <= 1'b0;
                     end
                 end
 
                 STATE_LENGTH: begin
-                    crc_update <= (count[0] != 1'd0);
-                    tx_first   <= 1'b0;
-                    tx_last    <= 1'b0;
-                    tx_data    <= length[7:0];
-                    tx_valid   <= 1'b1;
-                    if (count[0] == 1'd1) begin
-                        state   <= STATE_PAYLOAD;
-                        count   <= '0;
-                        tx_data <= length[15:8];
+                    st0_first <= 1'b0;
+                    st0_last  <= (st0_count[0] == 1'd0);
+                    if (st0_last) begin
+                        st0_state <= STATE_PAYLOAD;
+                        st0_count <= '0;
+                        st0_first <= 1'b1;
+                        st0_last  <= st0_length == 16'd0;
                     end
                 end
 
                 STATE_PAYLOAD: begin
-                    tx_first <= 1'b0;
-                    tx_last  <= 1'b0;
-                    tx_data  <= s_data;
-                    tx_valid <= 1'b1;
+                    st0_length <= st0_length_next;
+                    st0_first  <= 1'b0;
+                    st0_last   <= (st0_length_next == 0);
                     if (s_last) begin
-                        state <= STATE_PADDING;
+                        st0_state <= STATE_PADDING;
                     end
-                    if (count_next == length) begin
-                        state <= STATE_FCS;
-                        count <= '0;
+                    if (st0_last) begin
+                        st0_state  <= STATE_FCS;
+                        st0_count  <= '0;
+                        st0_length <= 'x;
+                        st0_first  <= 1'b1;
+                        st0_last   <= 1'b0;
                     end
                 end
 
                 STATE_PADDING: begin
-                    tx_first <= 1'b0;
-                    tx_last  <= 1'b0;
-                    tx_data  <= 8'h00;
-                    tx_valid <= 1'b1;
-                    if (count_next == length) begin
-                        state <= STATE_FCS;
-                        count <= '0;
+                    st0_length <= st0_length_next;
+                    st0_first  <= 1'b0;
+                    st0_last   <= (st0_length_next == 0);
+                    if (st0_last) begin
+                        st0_state  <= STATE_FCS;
+                        st0_count  <= '0;
+                        st0_length <= 'x;
+                        st0_first  <= 1'b1;
+                        st0_last   <= 1'b0;
                     end
                 end
 
                 STATE_FCS: begin
-                    crc_update <= 1'bx;
-                    tx_first   <= 1'b0;
-                    tx_last    <= (count == t_length'(3));
-                    tx_data    <= 'x;
-                    tx_valid   <= 1'b1;
-                    if (tx_last) begin
-                        state    <= STATE_IDLE;
-                        count    <= 'x;
-                        tx_first <= 'x;
-                        tx_last  <= 'x;
-                        tx_data  <= 'x;
-                        tx_valid <= 1'b0;
+                    st0_length <= 'x;
+                    st0_first  <= 1'b0;
+                    st0_last   <= st0_count == 3'd2;
+                    if (st0_last) begin
+                        st0_state <= STATE_IDLE;
+                        st0_count <= 'x;
                     end
                 end
 
                 default: begin
-                    state      <= STATE_IDLE;
-                    count      <= 'x;
-                    length     <= 'x;
-                    crc_update <= 'x;
-                    tx_first   <= 1'bx;
-                    tx_last    <= 1'bx;
-                    tx_data    <= 'x;
-                    tx_valid   <= 1'b1;
+                    st0_state  <= STATE_IDLE;
+                    st0_count  <= 'x;
+                    st0_length <= 'x;
+                    st0_first  <= 1'bx;
+                    st0_last   <= 1'bx;
                 end
             endcase
 
-            if ((tx_valid && !tx_last && in_cancel) || (state == STATE_PAYLOAD && !s_valid) || (state == STATE_PAYLOAD && count_next == length && !s_last)) begin
-                state      <= STATE_ERROR;
-                count      <= 'x;
-                length     <= 'x;
-                crc_update <= 'x;
-                tx_first   <= 1'b0;
-                tx_last    <= 1'b1;
-                tx_data    <= 8'h00;
-                tx_valid   <= 1'b1;
+            // エラーチェック
+            if ((st0_state == STATE_PAYLOAD && !s_valid) // 転送中アンダーフロー
+             || (st0_state == STATE_PAYLOAD && st0_last && !s_last)) begin // オーバーフロー
+                st0_state  <= STATE_ERROR;
+                st0_count  <= 'x;
+                st0_length <= 'x;
+                st0_first  <= 1'bx;
+                st0_last   <= 1'bx;
+            end
+
+            if (tx_cancel) begin
+                st0_state  <= STATE_IDLE;
+                st0_count  <= 'x;
+                st0_length <= 'x;
+                st0_first  <= 1'bx;
+                st0_last   <= 1'bx;
             end
         end
     end
 
-    assign s_ready = cke && (state == STATE_PAYLOAD);
+    assign s_ready = cke && (st0_state == STATE_PAYLOAD);
 
+
+    // ----------------------------
+    //  stage 1
+    // ----------------------------
+
+    STATE         st1_state;
+    logic         st1_first;
+    logic         st1_last ;
+    logic [8-1:0] st1_data ;
+    logic         st1_valid;
+
+    always_ff @ (posedge clk) begin
+        if (reset) begin
+            st1_state <= STATE_IDLE;
+            st1_first <= 'x;
+            st1_last  <= 'x;
+            st1_data  <= 'x;
+            st1_valid <= '0;
+        end else if (cke) begin
+
+            // stage1
+            st1_state <= st0_state;
+            st1_first <= st0_first;
+            st1_last  <= st0_last;
+            st1_data  <= 'x;
+            st1_valid <= 1'b0;
+
+            case (st0_state)
+                STATE_IDLE: begin
+                    // ここだけ追い越しして1cycle稼ぐ
+                    if (tx_start) begin
+                        st1_state <= STATE_PREAMBLE;
+                        st1_first <= 1'b1;
+                        st1_last  <= 1'b0;
+                        st1_data  <= 8'h55;
+                        st1_valid <= 1'b1;
+                    end
+                end
+
+                STATE_PREAMBLE: begin
+                    st1_data <= ((st0_last) ? (
+                        8'hd5
+                    ) : (
+                        8'h55
+                    ));
+                    st1_valid <= 1'b1;
+                end
+
+                STATE_LENGTH: begin
+                    st1_data <= ((st0_count[0]) ? (
+                        st0_length[15:8]
+                    ) : (
+                        st0_length[7:0]
+                    ));
+                    st1_valid <= 1'b1;
+                end
+
+                STATE_PAYLOAD: begin
+                    st1_data  <= s_data;
+                    st1_valid <= 1'b1;
+                end
+
+                STATE_PADDING: begin
+                    st1_data  <= 8'h00;
+                    st1_valid <= 1'b1;
+                end
+
+                STATE_FCS: begin
+                    st1_data  <= 'x;
+                    st1_valid <= 1'b1;
+                end
+
+                default: begin
+                end
+            endcase
+
+            // キャンセル
+            if (tx_cancel) begin
+                if (st1_valid && !(st1_last && st1_state == STATE_FCS)) begin
+                    st1_state <= STATE_ERROR;
+                    st1_first <= 1'b1;
+                    st1_last  <= 1'b1;
+                    st1_data  <= 8'h00;
+                    st1_valid <= 1'b1;
+                end else begin
+                    st1_state <= STATE_IDLE;
+                    st1_first <= 1'bx;
+                    st1_last  <= 1'bx;
+                    st1_data  <= 'x;
+                    st1_valid <= 1'b0;
+                end
+            end
+        end
+    end
+
+
+    // ----------------------------
+    //  stage 2
+    // ----------------------------
+
+    STATE          st2_state;
+    logic          st2_first;
+    logic          st2_last ;
+    logic [32-1:0] st2_crc  ;
+    logic [8-1:0]  st2_data ;
+    //    var st2_valid: logic    ;
+
+    always_ff @ (posedge clk) begin
+        if (reset) begin
+            st2_state <= STATE_IDLE;
+            st2_first <= 1'bx;
+            st2_last  <= 1'bx;
+            st2_data  <= 'x;
+            //            st2_valid = 1'b0;
+        end else if (cke) begin
+            st2_state <= st1_state;
+            st2_first <= st1_first;
+            st2_last  <= st1_last;
+            st2_data  <= st1_data;
+            //            st2_valid  = st1_valid;
+
+            if (tx_cancel) begin
+                st2_state <= STATE_IDLE;
+                st2_first <= 1'bx;
+                st2_last  <= 1'bx;
+                st2_data  <= 'x;
+                //                st2_valid = 1'b0;
+            end
+        end
+    end
 
     // CRC
-    logic [8-1:0]  crc_data ;
-    logic          crc_valid;
-    logic [32-1:0] crc_value;
-    assign crc_data  = tx_data;
-    assign crc_valid = tx_valid && (state == STATE_LENGTH || state == STATE_PAYLOAD || state == STATE_PADDING);
+    logic          crc_update;
+    logic [8-1:0]  crc_data  ;
+    logic          crc_valid ;
+    logic [32-1:0] crc_value ;
 
     jelly2_calc_crc #(
         .DATA_WIDTH (8           ),
@@ -203,30 +324,59 @@ module jellyvl_etherneco_tx (
         out_crc (crc_value)
     );
 
-    logic [32-1:0] out_data;
-    assign m_data   = out_data[7:0];
+    assign crc_update = !(st1_state == STATE_LENGTH && st1_first);
+    assign crc_data   = st1_data;
+    assign crc_valid  = (st1_state == STATE_LENGTH || st1_state == STATE_PAYLOAD || st1_state == STATE_PADDING);
+    assign st2_crc    = crc_value;
+
+
+
+    // ----------------------------
+    //  stage 3
+    // ----------------------------
+
+    STATE          st3_state;
+    logic          st3_first;
+    logic          st3_last ;
+    logic [32-1:0] st3_data ;
+    logic          st3_valid;
 
     always_ff @ (posedge clk) begin
         if (reset) begin
-            out_data <= 'x;
-            m_first  <= 'x;
-            m_last   <= 'x;
-            m_valid  <= 1'b0;
+            st3_state <= STATE_IDLE;
+            st3_first <= 1'bx;
+            st3_last  <= 1'bx;
+            st3_data  <= 'x;
+            st3_valid <= 1'b0;
+
         end else if (cke) begin
-            if (state == STATE_FCS) begin
-                if (count[1:0] == '0) begin
-                    out_data <= crc_value;
+            st3_state <= st2_state;
+            st3_first <= st2_first && st2_state == STATE_PREAMBLE;
+            st3_last  <= st2_last && st2_state == STATE_FCS;
+            if (st2_state == STATE_FCS) begin
+                if (st2_first) begin
+                    st3_data <= st2_crc;
                 end else begin
-                    out_data <= out_data >> 8;
+                    st3_data <= st3_data >> (8);
                 end
             end else begin
-                out_data <= {24'hxxxxxx, tx_data};
+                st3_data <= st2_data;
             end
+            st3_valid <= (st2_state != STATE_IDLE);
 
-            m_first <= tx_first;
-            m_last  <= tx_last;
-            m_valid <= tx_valid;
+            if ((tx_cancel && st3_valid && !st3_last) || st2_state == STATE_ERROR) begin
+                st3_state <= STATE_ERROR;
+                st3_first <= 1'b0;
+                st3_last  <= 1'b1;
+                st3_data  <= 8'h00;
+                st3_valid <= 1'b1;
+            end
         end
     end
+
+    assign m_first = st3_first;
+    assign m_last  = st3_last;
+    assign m_data  = st3_data[7:0];
+    assign m_valid = st3_valid;
 
 endmodule
